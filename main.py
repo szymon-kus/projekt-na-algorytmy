fce4547a72591f103dcc19812422c81f7c6323cf
PrzemekO84 <przemekorzechowski53@gmail.com>
2024-02-04 13:49:42 +0100

    Dodanie komentarza + ostatnie poprawki

--- main.py
+++ main.py
@@ -1,66 +1,80 @@
 from flask import Flask, request, jsonify, send_file
 import os
 import base64
-from managefiles import CryptoFileManager
+from managefiles import Key_Encrypt_Decrypt
 import logging
 
+#inicjalizacja flaska
 app = Flask(__name__)
 
-crypto_manager = CryptoFileManager(upload_folder='uploaded_files', key_file_path='secret.key')
+#Wykorzystanie klasy do stworzenia sciezki gdzie beda trzymane enkrypotwane i dekryptowane pliki oraz sciezka klucza
+crypto_manager = Key_Encrypt_Decrypt(upload_folder='uploaded_files', key_file_path='secret.key')
 
+#Endpoint do przesylania pliku i jego enkrypcji
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    #Obsluga bledow
     try:
+        #Sprawdzenie czy plik zostal przesladny
         if 'file' not in request.files:
             return jsonify({'error': 'No file part'}), 400
 
         file = request.files['file']
 
+        #Sprawdzenie czy nazwa pliku jest pusta
         if file.filename == '':
             return jsonify({'error': 'No selected file'}), 400
 
-        file_path = os.path.join(crypto_manager.upload_folder, file.filename)
-        file.save(file_path)
+        original_data = file.read()
+        encrypted_data = crypto_manager.encrypt(original_data)
 
-        with open(file_path, 'rb') as original_file:
-            original_data = original_file.read()
-            encrypted_data = crypto_manager.encrypt(original_data)
-
+        #Zapisanie enKryptowanego pliku na sciezce upload_folder 
         encrypted_file_path = os.path.join(crypto_manager.upload_folder, 'encrypted_' + base64.urlsafe_b64encode(file.filename.encode()).decode() + '.bin')
         with open(encrypted_file_path, 'wb') as encrypted_file:
             encrypted_file.write(encrypted_data)
 
+        #infomacja o prawidlowym dodaniu pliku i jego enkrypcji
         return jsonify({'message': 'File uploaded and encrypted successfully'})
+    
+    #oblsuga bledu w przypadku przesylania pliku
     except Exception as e:
         app.logger.error(f"Error during file upload: {str(e)}")
         return jsonify({'error': 'Internal Server Error'}), 500
 
+#Endpoint do pobrania pliku po dekrypcji
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
+    #obsluga bledow
     try:
         encrypted_file_path = os.path.join(crypto_manager.upload_folder, filename)
 
+        #Sprawdzenie czy plik istnieje
         if not os.path.exists(encrypted_file_path):
             app.logger.error(f"File not found: {filename}")
             return jsonify({'error': 'File not found'}), 404
 
+        #Binarne odczytanie enkryptowanego pliku
         with open(encrypted_file_path, 'rb') as encrypted_file:
             encrypted_data = encrypted_file.read()
 
+        #Dekrypcja enkryptowanego pliku
         decrypted_data = crypto_manager.decrypt(encrypted_data)
 
+        #Zapisanie odszyfrowanego pliku na sciezce upload folder
         decrypted_file_path = os.path.join(crypto_manager.upload_folder, 'decrypted_' + filename)
         with open(decrypted_file_path, 'wb') as decrypted_file:
             decrypted_file.write(decrypted_data)
 
+        #zworcenie odszyfrowanego pliku 
         return send_file(decrypted_file_path, as_attachment=True)
+    
+    #obsluga bledu podczas pobierania pliku
     except Exception as e:
         app.logger.error(f"Error during file download: {str(e)}")
         return jsonify({'error': 'Internal Server Error'}), 500
-    finally:
-        if 'decrypted_file_path' in locals() and os.path.exists(decrypted_file_path):
-            os.remove(decrypted_file_path)
 
+
+#Uruchomienie aplikacji
 if __name__ == '__main__':
     logging.basicConfig(filename='app.log', level=logging.ERROR)
 
--- managefiles.py
+++ managefiles.py
@@ -1,16 +1,19 @@
 import os
 from cryptography.fernet import Fernet
 
-class CryptoFileManager:
+#Storzenie klasy do generacji klucza, enkryptowania pliku i jego dekryptowania
+class Key_Encrypt_Decrypt:
     def __init__(self, upload_folder, key_file_path):
         self.upload_folder = upload_folder
         self.key_file_path = key_file_path
 
+        #Sprawdzenie czy istnieje folder z plikami
         if not os.path.exists(self.upload_folder):
             os.makedirs(self.upload_folder)
 
         self.key = self.load_or_generate_key()
 
+    #Tworzenie klucza z biblioteka Fernet crypotgrpahy (klucz symetryczny)
     def generate_key_and_save_to_file(self):
         key = Fernet.generate_key()
 
@@ -20,6 +23,7 @@ class CryptoFileManager:
         print("Key was generated and saved in file 'secret.key'.")
         return key
 
+    #Sprawdzenie czy klucz juz istnieje
     def load_or_generate_key(self):
         if os.path.exists(self.key_file_path):
             with open(self.key_file_path, 'rb') as key_file:
@@ -29,12 +33,14 @@ class CryptoFileManager:
 
         return key
 
-    def encrypt(self, data):
+    #Enkryptowanie pliku
+    def encrypt(self, file_to_encrypt):
         cipher = Fernet(self.key)
-        encrypted_data = cipher.encrypt(data)
+        encrypted_data = cipher.encrypt(file_to_encrypt)
         return encrypted_data
 
-    def decrypt(self, data):
+    #Dekrypcja pliku
+    def decrypt(self, file_to_decrypt):
         cipher = Fernet(self.key)
-        decrypted_data = cipher.decrypt(data)
+        decrypted_data = cipher.decrypt(file_to_decrypt)
         return decrypted_data
